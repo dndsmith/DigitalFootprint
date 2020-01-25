8752d1278c498a77cfeec2c0343c80c33ebc12f3
WaYnE041 <wayneekeh@gmail.com>
2020-01-25 16:32:06 -0500

    Update DigitalFootprint.py

--- DigitalFootprint.py
+++ DigitalFootprint.py
@@ -13,7 +13,7 @@ c.Username = "realDonaldTrump"
 print(get_retweeters_list("realDonaldTrump", 1221140946320084995))
 
 def get_retweeters_list(username, tweet_id):
-    r = requests.get('https://twitter.com/i/activity/retweeted_popup?id='+tweet_id)
+    r = requests.get('https://twitter.com/i/activity/favorites_popup?id='+tweet_id)
     text = r.text
     x = re.findall('div class=\\\\"account  js-actionable-user js-profile-popup-actionable \\\\" data-screen-name=\\\\"(.+?)\\\\" data-user-id=\\\\"', text)
     return x
