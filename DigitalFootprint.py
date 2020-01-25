70bfb3f0dcedbaae80658be3764a9449bb5e1f3a
Daniel Smith <dnls1297@gmail.com>
2020-01-25 16:30:08 -0500

    pls wayne

--- DigitalFootprint.py
+++ DigitalFootprint.py
@@ -10,6 +10,8 @@ c = twint.Config()
 c.Username = "realDonaldTrump"
 #twint.run.Search(c)
 
+print(get_retweeters_list("realDonaldTrump", ))
+
 def get_retweeters_list(username, tweet_id):
     r = requests.get('https://twitter.com/i/activity/retweeted_popup?id='+tweet_id)
     text = r.text
