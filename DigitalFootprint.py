dea72f2b47cbc53e296352f46825da3c8d45ab6c
WaYnE041 <wayneekeh@gmail.com>
2020-01-25 18:01:56 -0500

    Update DigitalFootprint.py

--- DigitalFootprint.py
+++ DigitalFootprint.py
@@ -51,4 +51,4 @@ def get_tweets_list(username):
     return tweets_df
 
 #print(get_retweeters_list("realDonaldTrump", "1221140946320084995"))
-print(get_tweets_list("reatDonaldTrump))
+print(get_tweets_list("realDonaldTrump"))
